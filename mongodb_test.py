32414b21dbc748a7cd10d4da1859b2bc5d894811
coconut750750 <sourbrandon@gmail.com>
2017-10-28 00:33:19 -0500

    added add and retrieve method for mongodb

--- mongodb_test.py
+++ mongodb_test.py
@@ -1,3 +1,22 @@
 from pymongo import MongoClient
 client = MongoClient()
 client = MongoClient('localhost', 27017)
+
+db = client.pymongo_test
+
+def add(dic):
+	posts = db.posts
+	result = posts.insert_one(dic)
+	print('One post: {0}'.format(result.inserted_id))
+
+def retrieve(params, posts):
+	post = posts.find_one(params)
+	print(post)
+
+post_data = {
+	'title': 'Python and MongoDB',
+	'content': 'PyMongo is fun, you guys',
+	'author': 'Brandon'
+}
+
+retrieve_data = {'author': 'Brandon'}
