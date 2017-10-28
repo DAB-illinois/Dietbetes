5ec09095671a39ff182ca845c1d269a067366530
coconut750750 <sourbrandon@gmail.com>
2017-10-28 01:13:27 -0500

    test read local files

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,23 +2,28 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
 
-from pymongo import MongoClient
-client = MongoClient()
-client = MongoClient('localhost', 27017)
-DATABASE_NAME = "databetes_app"
-TEST_DATABASE_NAME = "pymongo_test"
+#run flask on linux amazon http://www.datasciencebytes.com/bytes/2015/02/24/running-a-flask-app-on-aws-ec2/
 
-db = client[TEST_DATABASE_NAME]
+# from pymongo import MongoClient
+# client = MongoClient()
+# client = MongoClient('localhost', 27017)
+# DATABASE_NAME = "databetes_app"
+# TEST_DATABASE_NAME = "pymongo_test"
 
-def retrieve(params, posts):
-	post = posts.find_one(params)
-	return post
+# db = client[TEST_DATABASE_NAME]
+
+# def retrieve(params, posts):
+# 	post = posts.find_one(params)
+# 	return post
 
 @app.route('/')
 def my_form():
-	doc = retrieve({"author":"Brandon"}, db.posts)
-	print(doc)
-	return render_template("index.html", title=doc["title"], author=doc["author"])
+	# doc = retrieve({"author":"Brandon"}, db.posts)
+	# print(doc)
+	title = ""
+	with open("./templates/index.html","r") as f:
+		title = str(f.readlines())
+	return render_template("index.html", title=title)
 
 if __name__ == "__main__":
 	app.run(debug=True)
