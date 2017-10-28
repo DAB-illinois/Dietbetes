99bf753c7b0bd1ccbc76f030a5e24b57c01ae28d
coconut750750 <sourbrandon@gmail.com>
2017-10-28 04:02:17 -0500

    added try catch

--- scrape_gi.py
+++ scrape_gi.py
@@ -1,10 +1,12 @@
 from urllib.request import Request, urlopen
+import urllib.error
+import threading
 
 base_url = "http://www.glycemicindex.com/foodSearch.php?ak=detail&num="
 
 MAX_INDEX = 2688
-NAME_STRING = b'\t\t\t\t\t\t\t\t\t\t\t\t\t<td width="25%"><strong>Food Name</strong></td>\n'
-GI_STRING = b'\t\t\t\t\t\t\t\t\t\t\t\t\t<td><strong>GI (vs Glucose)</strong></td>\n'
+NAME_STRING = 211
+GI_STRING = 220
 
 from pymongo import MongoClient
 client = MongoClient()
@@ -18,27 +20,27 @@ def add_to_db(dic):
 	result = table.insert_one(dic)
 	print('One post: {0}'.format(result.inserted_id))
 
-def get_start_end_index(html, string_to_find):
-	string_index = html.index(string_to_find)
-	string_html = html[string_index + 1].decode()
+def get_start_end_index(html, index_to_get):
+	string_html = html[index_to_get].decode()
 	start = string_html.index(">")
 	end = string_html[start:].index("<") + start
-	return start, end, string_html
+
+	return string_html[start + 1:end]
 
 gi_data = {}
 
-for i in range(1, MAX_INDEX):
+for i in range(274, MAX_INDEX):
 	print(i)
 	url = base_url + str(i)
 
-	req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-	urlRequest = urlopen(req)
-	html = urlRequest.readlines()
+	try:
+		urlRequest = urlopen(Request(url))
+		html = urlRequest.readlines()
+	except urllib.error.HTTPError:
+		continue
 
-	start, end, item_html = get_start_end_index(html, NAME_STRING)
-	name = item_html[start + 1:end].split(",")[0]
-	start, end, item_html = get_start_end_index(html, GI_STRING)
-	gi_index = item_html[start + 1:end]
+	name = get_start_end_index(html, NAME_STRING).split(",")[0]
+	gi_index = get_start_end_index(html, GI_STRING)
 
 	if name in gi_data:
 		gi_data[name].append(gi_index)
