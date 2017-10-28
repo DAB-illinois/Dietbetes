097e87e80687e7f93b76a245822bf077bb02b60d
coconut750750 <sourbrandon@gmail.com>
2017-10-28 00:44:43 -0500

    added pymongo to flask

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,9 +2,22 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
 
+from pymongo import MongoClient
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+DATABASE_NAME = "databetes_app"
+TEST_DATABASE_NAME = "pymongo_test"
+
+db = client[TEST_DATABASE_NAME]
+
+def retrieve(params, posts):
+	post = posts.find_one(params)
+	return post
+
 @app.route('/')
 def my_form():
-	return render_template("index.html")
+	doc = retrieve({"author":"Brandon"}, db.posts)
+	return render_template("index.html", title=doc["title"], author=doc["author"])
 
 if __name__ == "__main__":
 	app.run(debug=True)
