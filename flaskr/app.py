ee0eb4f56152010eab55f1495c31e7f2f44a3429
coconut750750 <sourbrandon@gmail.com>
2017-10-27 22:44:56 -0500

    added flask

---
+++ flaskr/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file , flaskr.py
+
+@app.route('/')
+def my_form():
+	return render_template("layout.html")
