74da97550042421251f24a1ad6c98b0a35dfc777
coconut750750 <sourbrandon@gmail.com>
2017-10-28 15:03:13 -0500

    fixed getting carbs

--- flaskr/fatsecret_crawl.py
+++ flaskr/fatsecret_crawl.py
@@ -1,13 +1,13 @@
-from urllib.request import Request, urlopen
+import urllib
 
-LOOK_FOR = b'\t\t\t\t\t\t\t\t\t\t<div class="factTitle">Carbs</div>\r\n'
+LOOK_FOR = '\t\t\t\t\t\t\t\t\t\t<div class="factTitle">Carbs</div>\r\n'
 
 def get_carb_per_serving(url):
-	urlRequest = urlopen(Request(url))
-	html = urlRequest.readlines()
+	html = urllib.urlopen(url)
+	html = html.readlines()
 	html_str = str(html)
-	ind = html.index(LOOK_FOR)+1
-	start = html[ind].decode().index(">")+1
-	carbs = html[ind].decode()[start:]
-	carbs = carbs[:carbs.index("g<")]
-	print(carbs)
+	item = html[html.index(LOOK_FOR)+1]
+	item = item[item.index(">")+1:]
+	result = item[:item.index("g<")]
+	
+	return float(result)
