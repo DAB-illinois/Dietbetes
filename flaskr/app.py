ab6f0eae69018ce02f35c439cd698434dacc613a
coconut750750 <sourbrandon@gmail.com>
2017-10-28 01:29:18 -0500

    added pymonog back

--- flaskr/app.py
+++ flaskr/app.py
@@ -4,26 +4,23 @@ app.config.from_object(__name__) # load config from this file , flaskr.py
 
 #run flask on linux amazon http://www.datasciencebytes.com/bytes/2015/02/24/running-a-flask-app-on-aws-ec2/
 
-# from pymongo import MongoClient
-# client = MongoClient()
-# client = MongoClient('localhost', 27017)
-# DATABASE_NAME = "databetes_app"
-# TEST_DATABASE_NAME = "pymongo_test"
+from pymongo import MongoClient
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+DATABASE_NAME = "databetes_app"
+TEST_DATABASE_NAME = "pymongo_test"
 
-# db = client[TEST_DATABASE_NAME]
+db = client[TEST_DATABASE_NAME]
 
-# def retrieve(params, posts):
-# 	post = posts.find_one(params)
-# 	return post
+def retrieve(params, posts):
+	post = posts.find_one(params)
+	return post
 
 @app.route('/')
 def my_form():
-	# doc = retrieve({"author":"Brandon"}, db.posts)
-	# print(doc)
-	title = ""
-	with open("/var/www/html/flaskapp/templates/index.html","r") as f:
-		title = str(f.readlines())
-	return render_template("index.html", title=title)
+	doc = retrieve({"author":"Brandon"}, db.posts)
+	print(doc)
+	return render_template("index.html", title=doc["title"], author=doc["author"])
 
 if __name__ == "__main__":
 	app.run(debug=True)
